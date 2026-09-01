ea43c7b5644fb2ea9045c6d9ebb83938b3f95ea4
CarCVroom <bjornarlundm@gmail.com>
2026-09-01 14:59:22 +0200

    Sort.c still does not work

--- CWASM/hoffman_coding/main.c
+++ CWASM/hoffman_coding/main.c
@@ -3,7 +3,8 @@
 #include "sort.h"
 
 int main(void) {
-	char input[] = "AABBBBBBBBBBBBBBbbbbbCCCCCCCddddeeF"; 
+	//char input[] = "AABBBBBBBBBBBBBBbbbbbCCCCCCCddddeeF"; 
+	char input[] = "bccfffcddabddefeeeffe"; 
 
 	int result = frequency_count(input);	
 	if (result != SUCCESS) {
--- CWASM/hoffman_coding/sort.c
+++ CWASM/hoffman_coding/sort.c
@@ -5,9 +5,9 @@
 
 int partition(Character *characters, int *l, int *r) {
 	int pivot = characters[*r].frequency;	
-	printf("%d\n", pivot);
+	printf("Pivot: %d\n", pivot);
 	int i = *l - 1;
-	for (int j = 0; j < *r - 1; j++) {
+	for (int j = 0; j < *r ; j++) {
 		if (characters[j].frequency < pivot) {
 			i++;		
 			//printf("%d %d\n", j, i);
@@ -21,23 +21,27 @@ int partition(Character *characters, int *l, int *r) {
 	Character temp2 = characters[*r];
 	characters[*r] = characters[i + 1];
 	characters[i + 1] = temp2;
+	// Character temp2 = characters[*r];
+	// characters[*r] = characters[i + 1];
+	// characters[i + 1] = temp2;
 	//printf("%c, %d\n", temp2.charName, temp2.frequency);
 	//free(temp2);
 
-	printf("i before: %d\n", i);
 	return i + 1;
 }
 
 int sort(Character *characters, int chararcters_count, int *l, int *r) {
 	if (*l >= *r) {
+		printf("%d, %d\n", *l, *r);
 		return SUCCESS;
 	}
 
 	int p = partition(characters, l, r);
-	printf("p: %d\n", p);
+	printf("p: %d\n\n", p);
 	int p1 = p - 1;
 	int p2 = p + 1;
+	printf("%d 1 %d 2\n", p1, p2);
 	
-	// sort(characters, chararcters_count, l, &p1);
-	// sort(characters, chararcters_count, &p2, r);
+	sort(characters, chararcters_count, l, &p1);
+	//sort(characters, chararcters_count, &p2, r);
 }
