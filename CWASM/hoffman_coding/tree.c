125c47d77b9576bf4cabee74cb37747102848a59
CarCVroom <bjornarlundm@gmail.com>
2026-09-08 12:56:42 +0200

    More progress on tree.c. Almost done maybe idk

--- CWASM/hoffman_coding/tree.c
+++ CWASM/hoffman_coding/tree.c
@@ -7,9 +7,38 @@ int make_huffman_tree(Character **characters, int *chararcters_count, Node **nod
 	Node *local_nodes = NULL;
     	size_t local_count = 0;
 
-	while ((*characters)) {
+	Node node = {
+		.frequency = (*characters)[0].frequency + (*characters)[1].frequency
+	};
+
+	local_count++;
+	Node *tmpN = realloc(local_nodes, local_count * sizeof(Node));
+	if (tmpN == NULL) {
+		free(local_nodes);
+		return ERR_ALLOC;
+	}
+	local_nodes = tmpN;
+	local_nodes[local_count - 1] = node;
+
+	printf(" %d\n", local_nodes[0].frequency);
+
+	// Removes elements from chararcters
+	for(int j = 0; j < 2; ++j) {
+		for (int i = 0; i < *chararcters_count - 1; i++) {
+		    (*characters)[i] = (*characters)[i + 1];
+		}
+		(*chararcters_count)--;
+	}
+			
+	for (int k = 0; k < *chararcters_count; ++k) {
+		printf("%c, %d\n", (*characters)[k].charName, (*characters)[k].frequency);
+	}
+
+		// Add node back into the array
+
+	while ((*chararcters_count) > 0 ) {
 		Node node = {
-			.frequency = (*characters)[0].frequency + (*characters)[1].frequency
+			.frequency = local_nodes[local_count - 1].frequency + (*characters)[0].frequency
 		};
 
 		local_count++;
@@ -21,22 +50,16 @@ int make_huffman_tree(Character **characters, int *chararcters_count, Node **nod
 		local_nodes = tmpN;
 		local_nodes[local_count - 1] = node;
 
-		printf(" %d\n", local_nodes[0].frequency);
-
-		for(int j = 0; j < 2; ++j) {
-			for (int i = 0; i < *chararcters_count - 1; i++) {
-			    (*characters)[i] = (*characters)[i + 1];
-			}
-			(*chararcters_count)--;
+		for (int i = 0; i < *chararcters_count - 1; i++) {
+		    (*characters)[i] = (*characters)[i + 1];
 		}
-				
-		for (int k = 0; k < *chararcters_count; ++k) {
-			printf("%c, %d\n", (*characters)[k].charName, (*characters)[k].frequency);
-		}
+		(*chararcters_count)--;
 
-		// Add node back into the array
-		
-		return SUCCESS;
+		//return SUCCESS;
+	}
+
+	for (int k = 0; k < local_count; ++k) {
+		printf("%d\n", local_nodes[k].frequency); // THIS WORKS WHAT THE FUCK
 	}
 
 	return SUCCESS;
